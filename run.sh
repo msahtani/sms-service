fd821d2285909254eb7de481d1cdef62bcc4ab96
mohcine sahtani <mohcine.sahtani@gmail.com>
2024-12-17 23:51:18 +0100

    fixing

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ PID=`lsof -ti tcp:${PORT}`
 
 if [ -n "$PID" ]; then
   echo "Stopping existing app running on port ${PORT} (PID: ${PID})"
-  kill -15 $PID
+  kill $PID && sleep 10
 fi
 
 # Start the new instance
