4fd8946196cd421bb407d52c4dc049ce01f0e401
mohcine sahtani <mohcine.sahtani@gmail.com>
2024-12-18 08:45:48 +0100

    fixed:update close event

--- src/main/java/ma/mohcine/sms_service/SmsServiceApplication.java
+++ src/main/java/ma/mohcine/sms_service/SmsServiceApplication.java
@@ -1,18 +1,7 @@
 package ma.mohcine.sms_service;
 
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.catalina.Executor;
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.ContextClosedEvent;
-
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 
@@ -27,49 +16,6 @@ public class SmsServiceApplication {
 		SpringApplication.run(SmsServiceApplication.class, args);
 	}
 
-	@Bean
-	public GracefulShutdown gracefulShutdown(SseService sseService){
-		return new GracefulShutdown(sseService);
-	}
 
-	@RequiredArgsConstructor
-	private static class GracefulShutdown implements TomcatConnectorCustomizer,
-            ApplicationListener<ContextClosedEvent> {
-
-
-        private volatile Connector connector;
-
-		private final SseService sseService;
-
-        @Override
-        public void customize(Connector connector) {
-            this.connector = connector;
-        }
-
-        @Override
-        public void onApplicationEvent(ContextClosedEvent event) {
-
-			// start cleaning up
-			log.info("cleaning up SSE connections ... ");
-        	sseService.cleanUp();
-
-
-            this.connector.pause();
-            Executor executor = (Executor) this.connector.getProtocolHandler().getExecutor();
-            if (executor instanceof ThreadPoolExecutor) {
-                try (var threadPoolExecutor = (ThreadPoolExecutor) executor){
-                    threadPoolExecutor.shutdown();
-                    if (!threadPoolExecutor.awaitTermination(30, TimeUnit.SECONDS)) {
-                        log.warn("Tomcat thread pool did not shut down gracefully within "
-                                + "30 seconds. Proceeding with forceful shutdown");
-                    }
-                }
-                catch (InterruptedException ex) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-        }
-
-    }
-
+	
 }
