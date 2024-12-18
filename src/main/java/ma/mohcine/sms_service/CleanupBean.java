7b01b11181e509acd6f0065f3ac588c6e68f69fe
mohcine sahtani <mohcine.sahtani@gmail.com>
2024-12-18 08:08:47 +0100

    added cleanup logging

--- src/main/java/ma/mohcine/sms_service/CleanupBean.java
+++ src/main/java/ma/mohcine/sms_service/CleanupBean.java
@@ -4,16 +4,18 @@ import org.springframework.beans.factory.DisposableBean;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @Component
 @RequiredArgsConstructor
+@Log4j2
 public class CleanupBean implements DisposableBean{
 
     private final SseService sseService;
 
     @Override
     public void destroy() throws Exception {
-        
+        log.info("cleaning up SSE connections ... ");
         sseService.cleanUp();
     }
     
